55dbfff0a1ad356bddd9472c92d3c3864a439720
mahi-ayub <pikapi23lap@gmail.com>
2025-06-24 11:41:05 +0530

    File was deleted

--- test2.py
+++
@@ -1,8 +0,0 @@
-def is_prime(n):
-    return n > 1 and all(n % i != 0 for i in range(2, int(n**0.5) + 1))
-
-try:
-    num = int(input("Enter a number: "))
-    print(f"{num} is {'a prime' if is_prime(num) else 'not a prime'} number.")
-except ValueError:
-    print("Invalid input.")
